c679c86a2f8e75c91020545f35dc3de2cea69052
preetid16 <54541933+preetid16@users.noreply.github.com>
2025-04-16 23:59:21 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import laughing from ' ./assets/laughing.svg';
+import './style.css';
+import generateJoke from '/laughing-js';
+
+const laughImg = document. getElementById ("laughImg");
+laughImg.src = laughing;
+
+const joke = document.getElementById ("joke");
+joke.innerHTML = generateJoke();
+
+const jokeBtn = document-getElementById ("jokeBtn");
+jokeBtn.addEventListener('click', () => {
+    joke.innerHTML = generateJoke();
+});
---
+++ src/laughing.js
@@ -0,0 +1,9 @@
+import joke from './assets/json/jokes.json';
+
+const generateJoke = () => {
+// Get a random index within the bounds of the joke array
+const randomIndex = Math.floor(Math.random() * joke.length);
+// Return the joke at the random index
+return joke[randomIndex] ? joke[randomIndex]?.joke : 'I was wondering why the frisbee was getting bigger, then it hit me.'
+}
+export default generateJoke;
